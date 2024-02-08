ce760ffed68201d243d90288f1400a83640c9b2b
kevin-penkowski <kevin.penkowski@gmail.com>
2024-02-08 11:03:49 -0500

    Working command to get node ips

--- kubectl-plugin/main.go
+++ kubectl-plugin/main.go
@@ -4,34 +4,28 @@ import (
 	"fmt"
 	"net"
 	"os/exec"
-	"strings"
 	"syscall"
 )
 
 func main() {
-
-	command_get_pod_count := strings.Split("kubectl get pods -o jsonpath=\"{..nodeName}\" | tr -s '[[:space:]]' '\n' | sort | awk ", " ")
-	command_get_pod_count = append(command_get_pod_count, "'{print $2 \"\t\" $1}'") // Append this separately to avoid splitting it
-
-	command_get_node_ips := strings.Split("kubectl describe nodes | grep 'Name:\\|flannel.alpha.coreos.com/public-ip' | awk", " ")
-	command_get_node_ips = append(command_get_node_ips, "'{print $2}'") // Append this separately to avoid splitting it
-	command_get_node_ips = append(command_get_node_ips, strings.Split("| paste - -", " ")...)
-
-	cmd_get_pod_count := exec.Command(command_get_pod_count[0], command_get_pod_count[1:]...)
-	stdout1, err1 := cmd_get_pod_count.Output()
-	if err1 != nil {
-		fmt.Println(err1.Error())
+	// ----------------- Week 4 -----------------
+	// Get the public IPs of the nodes with count of number of pods running on each node
+	// Create a string bash command
+	kube_cmd := "for NODE in $(kubectl get pods -o jsonpath=\"{..nodeName}\" " +
+		"| tr -s '[[:space:]]' '\\n' | sort | awk '{print $2\"\\t\"$1}'); " +
+		"do kubectl describe nodes | grep 'Name:\\|flannel.alpha.coreos.com/public-ip' " +
+		"| awk '{print $2}' | paste - - | grep $NODE | awk '{print $2}'; done | tr -s '[[:space:]]' '\n'"
+	// Execute the bash command
+	test_command := exec.Command("bash", "-c", kube_cmd)
+	stdout, err := test_command.Output()
+	if err != nil {
+		fmt.Println(err.Error())
 		return
 	}
-	fmt.Println(string(stdout1))
-
-	cmd_get_node_ips := exec.Command(command_get_node_ips[0], command_get_node_ips[1:]...)
-	stdout2, err2 := cmd_get_node_ips.Output()
-	if err1 != nil {
-		fmt.Println(err2.Error())
-		return
-	}
-	fmt.Println(string(stdout2))
+	// Get command output as byte array
+	ips := stdout
+	fmt.Println(string(ips))
+	fmt.Println(ips)
 
 	// Construct a packet to send
 	pkt := []byte{
@@ -40,18 +34,20 @@ func main() {
 		0x16, 0x9b, 0x47, 0x4e, 0x47, 0x4c, // Source MAC (16:9B:47:4E:47:4C)
 		0x08, 0x00, // Type = IP
 		// IP header = 20 bytes
-		0x45, 0x00, 0x00, 0x3c, // Version, IHL, TOS, Length
+		0x45, 0x00, 0x00, 0x3c, // Version, IHL, TOS, Length TODO: Edit length
 		0x47, 0xd9, 0x40, 0x00, // ID, Flags, Fragment Offset
 		0x40, 0x06, 0xb5, 0x94, // TTL, Protocol, Header Checksum
 		0x0a, 0x00, 0x00, 0x01, // Source IP (10.0.0.2)
 		0x0a, 0x00, 0x00, 0x02, // Destination IP (10.0.0.1)
 		// UDP header = 8 bytes
 		0x75, 0x30, 0x75, 0x30, // Source Port (30000), Destination Port (30000), TODO: Subject to change
-		0x00, 0x09, 0x6b, 0xeb, // Length, Checksum
-		// Payload = 1 byte
-		0xFF, // Data
-		// Total length = 43 bytes
+		0x00, byte(len(ips)), 0x6b, 0xeb, // Length, Checksum
+		// Total length = 42 + length of ips bytes
 	}
+	// Append the public IPs to the packet as the payload
+	pkt = append(pkt, ips...)
+
+	// ----------------- Week 3 -----------------
 
 	// From https://stackoverflow.com/questions/35841275/sending-raw-packet-with-ethernet-header-using-go-language
 	fd, err := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, syscall.ETH_P_ALL)
